adcbcf5ec3c3c72ab980b549ed5b9cf5fe0fc4a9
KrishMathur <1mathurkri@hdsb.ca>
2024-06-11 09:24:17 -0400

    Create spotify.py

---
+++ spotify.py
@@ -0,0 +1,10 @@
+import pygame 
+
+def spotify():
+    from pygame import mixer 
+    pygame.init()
+    window = pygame.display.set_mode((1500, 1000 ))
+    pygame.display.set_caption('Sagar Music')
+
+    while True:
+        print("bruh")
