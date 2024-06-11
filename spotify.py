daf1a2d546e73003be47c425f6b51a9469dd068b
KrishMathur <1mathurkri@hdsb.ca>
2024-06-11 13:51:35 -0400

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -3,8 +3,14 @@ import pygame
 def spotify():
     from pygame import mixer 
     pygame.init()
+    mixer.init()
     window = pygame.display.set_mode((1500, 1000 ))
     pygame.display.set_caption('Sagar Music')
 
     while True:
-        print("bruh")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+                
+
+        pygame.display.flip()
